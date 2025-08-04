23611c1122a31c18cf9538789b7aaa8828bde7d7
h.gleam <h.gleam.1@gmail.com>
2025-08-04 17:39:39 +0900

    fix: usersテーブルからemail_verified_atカラムを削除
    主な変更内容：
    
    - `users`テーブルのマイグレーションファイルから、`email_verified_at`カラムを削除(別のマイグレーションファイルで定義しているため)。

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->text('profile_image_path')->nullable();
             $table->string('zip_code', 8)->nullable();
